74706e250ecefc1c7e712ae160a487dc0354015c
fengzhuojia <179309463@qq.com>
2018-01-18 22:21:38 +0800

    elasticsearch urls

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
-ENV["ELASTICSEARCH_URL"] = "http://localhost:9200,http://localhost:9201"
+ENV["ELASTICSEARCH_URL"] = "http://localhost:9200,http://localhost:9200"
 
 Searchkick.client_options = {
   retry_on_failure: true
